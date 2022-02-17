afd00df21b298a8fd170b6c3190b97ccb79911e8
esundbla <69823637+esundbla@users.noreply.github.com>
2022-02-17 09:19:45 -0700

    ml

--- MachineLearning/RegAssignment/RegDemo.py
+++ MachineLearning/RegAssignment/RegDemo.py
@@ -1,11 +1,14 @@
 import random
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 
-def sumation(H, Y, zed):
+def sumation(X,H, Y, zed):
     sum = 0
     if  not zed:
         for i in range(len(H)):
-            sum = sum + ((H[i] - Y[i])*H[i])
+            sum = sum + (((H[i] - Y[i])*X[i]))
         return sum
     else:
         for i in range(len(H)):
@@ -23,21 +26,35 @@ if __name__ == "__main__":
 
     w1 = 1.0
     b = 1.0
-    step = 0.0001
+    step = 0.00001
+    w1_hist =[]
+    b_hist=[]
 
-    for i in range(10000000):
+    for i in range(1000000):
         h = []
         for x in samples:
             h.append((w1 * x) + b)
-
-        w1 = w1 - step*(sumation(h, results, False))
-        b = b - step*(sumation(h, results, True))
+        w1_hist.append(w1)
+        b_hist.append(b)
+        w1 = w1 - step*(sumation(samples, h, results, False))
+        b = b - step*(sumation(samples, h, results, True))
 
     print("Y =", w1, "X +", b)
     print("done")
+    bb = np.arange(0, 100, 1)  # bias
+    ww = np.arange(0, 10, 0.1)  # weight
+    Z = np.zeros((len(bb), len(ww)))
 
-    machine = []
-    for x in samples:
-        machine.append((w1*x) + b)
-    print(results)
-    print(machine)
+    for i in range(len(bb)):
+        for j in range(len(ww)):
+            b = bb[i]
+            w = ww[j]
+            Z[j][i] = 0
+            for n in range(len(samples)):
+                Z[j][i] = Z[j][i] + (w * samples[n] + b - results[n]) ** 2  # this is the loss
+            Z[j][i] = Z[j][i] / len(results)
+
+    plt.contour(bb, ww, Z, 200, alpha=0.7, cmap=plt.get_cmap('jet'))
+    plt.plot([50], [2], 'x', ms=12, markeredgewidth=3, color='orange')
+    plt.plot(b_hist, w1_hist, 'o-', ms=3, lw=1.5, color='black')
+    plt.show()

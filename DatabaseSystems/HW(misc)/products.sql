49a6291742ee0b8c015dd236b181a11d7bfeffd8
esundbla <69823637+esundbla@users.noreply.github.com>
2022-04-06 10:20:27 -0600

    DB work update

--- DatabaseSystems/HW(misc)/products.sql
+++ DatabaseSystems/HW(misc)/products.sql
@@ -1,23 +1,30 @@
 --Erik Sundblad 
---Homework 6
+--Homework 7
 
 --Create temp database
 CREATE DATABASE temp;
+
+--Move to DB
+\c temp;
 
 --Create table Products
 CREATE TABLE Products(
     id SERIAL NOT NULL PRIMARY KEY,
     descr VARCHAR(30) NOT NULL,
-    price FLOAT NOT NULL,
-    cond  VARCHAR(10) NOT NULL DEFAULT ('new')
+    price FLOAT NOT NULL
 );
+
+--Adding Column Cond to 
+ALTER TABLE Products ADD cond VARCHAR(10) NOT NULL DEFAULT ('new');
 
 --Create condition set function
 CREATE FUNCTION cond_value_set() RETURNS TRIGGER
-    LANGUAGE plpgsql
+    LANGUAGE plpgsql 
     AS $$
         BEGIN
-            IF NEW.cond != 'used' THEN
+            IF LOWER(NEW.cond) = 'used' THEN
+                NEW.cond = 'used';
+            ELSE 
                 NEW.cond = 'new';
             END IF;
             RETURN NEW;
@@ -34,8 +41,8 @@ CREATE TRIGGER cond_value
 --Inserts
 INSERT INTO Products (descr, price, cond) VALUES ('Ninja Sword', 250, 'new');
 INSERT INTO Products (descr, price) VALUES ('Dummy', 50);
-INSERT INTO Products (descr, price, cond) VALUES ('Fake Blood', 5, 'used');
-INSERT INTO Products (descr, price, cond) VALUES ('Rubber Ducky', 1, 'used');
+INSERT INTO Products (descr, price, cond) VALUES ('Fake Blood', 5, 'USED');
+INSERT INTO Products (descr, price, cond) VALUES ('Rubber Ducky', 1, 'uSeD');
 INSERT INTO Products (descr, price, cond) VALUES ('Bathtub Soap', 3, 'used once');
 INSERT INTO Products (descr, price) VALUES ('Brazilian Coffee', 5);
 INSERT INTO Products (descr, price, cond) VALUES ('Running Shoes', 50, 'fair');

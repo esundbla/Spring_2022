bac50ae4bedebc869f2509d66135a369f7cfa514
esundbla <69823637+esundbla@users.noreply.github.com>
2022-04-28 08:34:26 -0600

    DB hw 9

--- DatabaseSystems/InClass/books.py
+++ DatabaseSystems/InClass/books.py
@@ -10,50 +10,48 @@ collection = db["books"]
 
 collection.insert_many([
 	{
-		"isbn" : 123456,
+		"isbn" : 978157965,
 		"title" : "The French Laundry",
 		"Author" : "Thomas Keller",
-		"date" : {"year": 1997,
+		"date" : {"year": 1999,
 				  "month": "September"},
 		"tags" : [
 			"Cooking"
 		]
-	},
-	{
-		"isbn": 123456,
-		"title": "The French Laundry",
-		"Author": "Thomas Keller",
-		"date": {"year": 1997,
-				 "month": "September"},
+	},{
+		"isbn" : 9781607745945,
+		"title" : "A New Napa Cuisine",
+		"Author" : "Christopher Kostow",
+		"date" : {"year": 2014,
+				 "month": "June"},
 		"tags": [
-			"Cooking"
-		]
+			"Cooking"]
 	},{
-		"isbn" : 123456,
-		"title" : "The French Laundry",
-		"Author" : "Thomas Keller",
-		"date" : {"year": 1997,
-				  "month": "September"},
+		"isbn": 10060585366 ,
+		"title" : "The Whole Beast",
+		"Author" : "Fergus Henderson",
+		"date" : {"year": 2004,
+				  "month": "May"},
 		"tags" : [
 			"Cooking"
 		]
 	},{
-		"isbn" : 123456,
-		"title" : "The French Laundry",
-		"Author" : "Thomas Keller",
-		"date" : {"year": 1997,
-				  "month": "September"},
+		"isbn" : 1584503262,
+		"title" : "Practical Java Game Programming",
+		"Author" : "Dustin Clingman",
+		"date" : {"year": 2004,
+				  "month": "February"},
 		"tags" : [
-			"Cooking"
+			"Programming"
 		]
 	},{
-		"isbn" : 123456,
-		"title" : "The French Laundry",
-		"Author" : "Thomas Keller",
-		"date" : {"year": 1997,
-				  "month": "September"},
+		"isbn" : 10399128964,
+		"title" : "Dune",
+		"Author" : "Frank Herbert",
+		"date" : {"year": 1984,
+				  "month": "August"},
 		"tags" : [
-			"Cooking"
+			"Sci-fi"
 		]
 	},
 ])
